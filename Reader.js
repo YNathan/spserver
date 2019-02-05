ea355e29a6c03ac118cad591af26a4d5745d1f18
YNathan <Nyi0527879217>
2019-02-05 19:02:00 +0200

    add by step dynamic params

--- Reader.js
+++ Reader.js
@@ -1,302 +1,231 @@
 var GoogleSpreadsheet = require('google-spreadsheet');
 var express = require("express");
+var device = require('express-device');
 var bodyParser = require('body-parser');
 var async = require('async');
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.use(device.capture());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-var responce = {
-    fix: "",
-    columns: [],
-    values: []
-}
-
-function initResponce() {
-    responce = {
-        fix: "",
-        columns: [],
-        values: []
-    }
-}
 
 // spreadsheet key is the long id in the sheets URL
 var doc = new GoogleSpreadsheet('1W9EK8bHkJgNLtKRlxnBlrw1smHPE5m0OJcR53aSPMbM');
-var sheet;
 
 var ColumnsNames = {
-    company : "מכשירשנכנסעלבעיהמסוימתותוךכדיהתיקוןנתגלתהבעיהנוספתיעוכבזמןהתיקוןשלו.איןאחריותעלתיקוניקורוזיה.ניקויקורוזיהלאכוללרכיביםוחלקיםנוספיםאחרים.תוכנהמחייבתמחיקתנתוניםהמעבדהאינהאחראיתעלשוםמידעבמכשיר.המחיריםכוללעבודהוכוללמעמ.שירותלעסקים050-437-2672זכותהקנייןהרוחניהואשלספיקרפוןכלהזכויותשמורות",
-    model : "_cokwr",
-    deviceName : "_cpzh4",
-    riskAndComments : "_cre1l",
-    price1 : "_ciyn3",
-    price2 : "_cyevm"
-
-}
-var screenRisk = "מכשירשנכנסעלבעיהמסוימתותוךכדיהתיקוןנתגלתהבעיהנוספתיעוכבזמןהתיקוןשלו.איןאחריותעלתיקוניקורוזיה.ניקויקורוזיהלאכוללרכיביםוחלקיםנוספיםאחרים.תוכנהמחייבתמחיקתנתוניםהמעבדהאינהאחראיתעלשוםמידעבמכשיר.המחיריםכוללעבודהוכוללמעמ.שירותלעסקים050-437-2672זכותהקנייןהרוחניהואשלספיקרפוןכלהזכויותשמורות";
-var screenCompany = "מכשירשנכנסעלבעיהמסוימתותוךכדיהתיקוןנתגלתהבעיהנוספתיעוכבזמןהתיקוןשלו.";
-
-var newVersionFixes = ["מסכים"];
+    company: 1,
+    model: 2,
+    deviceName: 3,
+    riskAndComments: 4,
+    price1: 5,
+    price2: 6
+};
 
 
-function isEqual(element, anotherOne) {
-    return element == anotherOne;
-}
+var request;
+var responce;
+var responceBody;
 
 function setAuth(step) {
     doc.useServiceAccountAuth(require('./credentials.json'), step);
 }
 
-getFixes = function (req, res) {
-    async.series([
-        setAuth,
-        function getInfoAndWorksheets() {
-            doc.getInfo(function (err, info) {
-                res.json(info.worksheets.map(o => o.title));
-            });
-        },
-    ], function (err) {
-        if (err) {
-            console.log('Error: ' + err);
-        }
-    });
-}
+function respond(step) {
+    try{
+        responce.json(responceBody);
+    }catch(error){
 
-getColumns = function (req, res) {
-    initResponce();
-    async.series([
-        setAuth,
-        function findFix() {
-            doc.getInfo(function (err, info) {
+        handleError(error);
+    }
+    step();
 
-                for (var i in info.worksheets) {
-                    if (info.worksheets[i].title == req.query.fix) {
-                        sheet = info.worksheets[i];
-                        info.worksheets[i].getCells({'min-row': 1,'max-row': 1,}, function (err, cells) {
-                            responce.fix = req.query.fix;
-                            responce.columns = cells.map(c => c.value);
-                            res.json(responce);
-                        });
-                    }
-                }
-            });
-        }
-    ], function (err) {
-        if (err) {
-            console.log('Error: ' + err);
-        }
-    });
 }
 
 function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
 
-getValueByColumns = function (req, res) {
-    initResponce();
-    var colIndex;
-    async.series([
-        setAuth,
-        function getValueFromRowAndCell() {
-            doc.getInfo(function (err, info) {
-                var rowParams = {
-                    title :  req.query.fix,
-                    offset: 1,
-                };
-                for (var wIdx in info.worksheets) {
-                    if (info.worksheets[wIdx].title == req.query.fix) {
-                        info.worksheets[wIdx].getRows(rowParams, function (err, rows) {
-                            var  a = rows.map(o => o[req.query.column]).filter( onlyUnique );
-                            res.json(rows.map(o => o[req.query.column]).filter( onlyUnique ));
-                        });
-                    }
-                }
-            });
-        }
-  /*  var a = [];
-    for(var i in rows){
-        if(rows[i]["חברה"] == "APPLE"){
-            a.push(rows[i]);
-        }
+function handleError(err) {
+    if (err) {
+        var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+        console.log("Error When User in IP: " + ip + ", With Device: " + request.device.type + ", request for: " + request.originalUrl.split("/api/")[1] + ".");
+        console.log('Error: ' + err);
     }
-*/
-], function (err) {
-        if (err) {
-            console.log('Error: ' + err);
-        }
-    });
 }
 
-function isNewVersion(fix) {
-    return newVersionFixes.includes(fix);
-}
+getFixes = function (req, res) {
+    var worksheetsTitles = [];
+    async.series([
+        setAuth,
+        function getSheetsTitles(step) {
+            doc.getInfo(function (err, info) {
+                worksheetsTitles = info.worksheets.map(o => o.title);
+                responceBody = worksheetsTitles;
+                step();
+            });
+        },
+        respond
+    ], function (err) {
+        handleError(err);
+    });
+};
+
 
 getCompany = function (req, res) {
-    initResponce();
+    var companiesNames = [];
     async.series([
         setAuth,
-        function getValueFromRowAndCell() {
+        function getValueFromRowAndCell(step) {
             doc.getInfo(function (err, info) {
                 var rowParams = {
-                    title :  req.query.fix,
+                    title: req.query.fix,
                     offset: 2,
                 };
                 for (var wIdx in info.worksheets) {
                     if (info.worksheets[wIdx].title == req.query.fix) {
                         info.worksheets[wIdx].getRows(rowParams, function (err, rows) {
-                           if(isNewVersion(req.query.fix)){
-                               res.json(rows.map(o => o[screenCompany]).filter( onlyUnique ));
-                           }else{
-                               res.json(rows.map(o => o[ColumnsNames.company]).filter( onlyUnique )); 
-                           }
-                           
+                            companiesNames = rows.map(o => fromRowToFieldAtIndex(o, ColumnsNames.company)).filter(onlyUnique);
+                            responceBody = companiesNames;
+                            step();
                         });
                     }
                 }
             });
-        }
+        },
+        respond
     ], function (err) {
-        if (err) {
-            console.log('Error: ' + err);
-        }
+        handleError(err);
     });
-}
+};
 
 
 getDeviceName = function (req, res) {
-    initResponce();
+    var devicesNames = [];
     async.series([
         setAuth,
-        function getValueFromRowAndCell() {
+        function getValueFromRowAndCell(step) {
             doc.getInfo(function (err, info) {
                 var rowParams = {
-                    title :  req.query.fix,
+                    title: req.query.fix,
                     offset: 2,
                 };
                 for (var wIdx in info.worksheets) {
                     if (info.worksheets[wIdx].title == req.query.fix) {
                         info.worksheets[wIdx].getRows(rowParams, function (err, rows) {
-                            var results = [];
-                            for(var rIdx in rows) {
-                                if (isNewVersion(req.query.fix)) {
-                                    if (rows[rIdx][screenCompany] == req.query.company) {
-                                        results.push(rows[rIdx][ColumnsNames.deviceName])
-                                    }
-                                } else {
-                                    if (rows[rIdx][ColumnsNames.company] == req.query.company) {
-                                        results.push(rows[rIdx][ColumnsNames.deviceName])
-                                    }
+                            for (var rIdx in rows) {
+                                if (fromRowToFieldAtIndex(rows[rIdx], ColumnsNames.company) == req.query.company) {
+                                    devicesNames.push(fromRowToFieldAtIndex(rows[rIdx], ColumnsNames.deviceName));
                                 }
                             }
-                            res.json(results.filter(onlyUnique));
+                            responceBody = devicesNames.filter(onlyUnique);
+                            step();
                         });
                     }
                 }
             });
-        }
+        },
+        respond
     ], function (err) {
-        if (err) {
-            console.log('Error: ' + err);
-        }
+        handleError(err);
     });
-}
+};
 
 getModel = function (req, res) {
-    initResponce();
+    var models = [];
     async.series([
         setAuth,
-        function getValueFromRowAndCell() {
+        function getValueFromRowAndCell(step) {
             doc.getInfo(function (err, info) {
                 var rowParams = {
-                    title :  req.query.fix,
+                    title: req.query.fix,
                     offset: 2,
                 };
                 for (var wIdx in info.worksheets) {
                     if (info.worksheets[wIdx].title == req.query.fix) {
                         info.worksheets[wIdx].getRows(rowParams, function (err, rows) {
-                            var results = [];
-                            for(var rIdx in rows) {
-                                if (isNewVersion(req.query.fix)) {
-                                    if (rows[rIdx][screenCompany] === req.query.company && rows[rIdx][ColumnsNames.deviceName] === req.query.devicename) {
-                                        results.push(rows[rIdx][ColumnsNames.model])
-                                    }
-                                } else {
-                                    if (rows[rIdx][ColumnsNames.company] == req.query.company && rows[rIdx][ColumnsNames.deviceName] === req.query.devicename) {
-                                        results.push(rows[rIdx][ColumnsNames.model])
-                                    }
+
+                            for (var rIdx in rows) {
+                                if (fromRowToFieldAtIndex(rows[rIdx], ColumnsNames.company) === req.query.company && fromRowToFieldAtIndex(rows[rIdx], ColumnsNames.deviceName) === req.query.devicename) {
+                                    models.push(fromRowToFieldAtIndex(rows[rIdx], ColumnsNames.model));
                                 }
                             }
-                            res.json(results.filter(onlyUnique));
+                            responceBody = models.filter(onlyUnique);
+                            step();
                         });
                     }
                 }
             });
-        }
+        },
+        respond
     ], function (err) {
-        if (err) {
-            console.log('Error: ' + err);
-        }
+        handleError(err);
     });
-}
+};
 
 getAll = function (req, res) {
-    initResponce();
+    var allResults = {
+        riskAndComments: "",
+        price: ""
+    }
     async.series([
         setAuth,
-        function getValueFromRowAndCell() {
+        function getValueFromRowAndCell(step) {
             doc.getInfo(function (err, info) {
                 var rowParams = {
-                    title :  req.query.fix,
+                    title: req.query.fix,
                     offset: 2,
                 };
                 for (var wIdx in info.worksheets) {
                     if (info.worksheets[wIdx].title == req.query.fix) {
                         info.worksheets[wIdx].getRows(rowParams, function (err, rows) {
-                            var found = false;
-                            var results = [];
-
-                            var allResults = {
-                                riskAndComments : "",
-                                price : ""
-                            }
-                            for(var rIdx in rows) {
-                                if (isNewVersion(req.query.fix)) {
-                                    if (rows[rIdx][screenCompany] === req.query.company && rows[rIdx][ColumnsNames.deviceName] === req.query.devicename && rows[rIdx][ColumnsNames.model] === req.query.model) {
-                                        found = true;
-                                        allResults.riskAndComments = rows[rIdx][screenRisk];
-                                        allResults.price = rows[rIdx][ColumnsNames.price2];
-                                        res.json(allResults);
-                                    }
-                                } else {
-                                    if (rows[rIdx][ColumnsNames.company] == req.query.company && rows[rIdx][ColumnsNames.deviceName] === req.query.devicename  && rows[rIdx][ColumnsNames.model] === req.query.model) {
-                                        found = true;
-                                        allResults.riskAndComments = rows[rIdx][ColumnsNames.riskAndComments];
-                                        allResults.price = rows[rIdx][ColumnsNames.price1];
-                                        res.json(allResults);
-                                    }
+                            for (var rIdx in rows) {
+                                if (fromRowToFieldAtIndex(rows[rIdx],ColumnsNames.company) === req.query.company && fromRowToFieldAtIndex(rows[rIdx],ColumnsNames.deviceName) === req.query.devicename && fromRowToFieldAtIndex(rows[rIdx],ColumnsNames.model) === req.query.model) {
+                                    allResults.riskAndComments = fromRowToFieldAtIndex(rows[rIdx],ColumnsNames.riskAndComments);
+                                    allResults.price = fromRowToFieldAtIndex(rows[rIdx],ColumnsNames.price1);
                                 }
                             }
-                            if(!found){
-                                res.json({});
-                            }
+                            responceBody = allResults;
+                            step();
                         });
                     }
                 }
             });
-        }
+        },
+        respond
     ], function (err) {
-        if (err) {
-            console.log('Error: ' + err);
-        }
+        handleError(err);
     });
+};
+
+function fromRowToFieldAtIndex(row, index) {
+    delete row._xml;
+    delete row._links;
+    delete row.del;
+    delete row.save;
+    delete row.__proto__;
+    var rowAsJson = JSON.parse(JSON.stringify(row));
+    var arr = [];
+    for (var i in rowAsJson) {
+        arr.push(rowAsJson[i]);
+    }
+    return arr[index];
 }
 
+
 var router = express.Router();
+
+router.get('*', function (req, res, next) {
+    request = req;
+    responce = res;
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log("User in IP: " + ip + ", With Device: " + req.device.type + ", request for: " + req.originalUrl.split("/api/")[1] + ".");
+    next();
+});
+
 router.get('/fixes', getFixes);
 router.get('/company', getCompany);
 router.get('/devicenames', getDeviceName);
@@ -309,6 +238,7 @@ router.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-
 app.use('/api', router);
 app.listen(5000);
+console.log("Server running on port: 5000");
+//settingTheColumns();
